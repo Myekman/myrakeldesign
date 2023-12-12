11609e5dfc4943537ec2a31f58fd140a718e4369
Myekman <my@traningstemplet.se>
2023-12-12 20:04:29 +0000

    detailpage

--- myrakel/src/components/PaintingDetail.js
+++ myrakel/src/components/PaintingDetail.js
@@ -17,27 +17,37 @@ const PaintingDetail = () => {
   return (
     <>
     <div>
+      <div className={style.container}>
         <div className={style.card}>
           <div className='card-body'>
-            <h1>{painting.title}</h1>
-            <div class="row">
-              <div class="col-lg-5 col-md-5 col-sm-6">
-                <div className='imagecontainer'>
-                    <img
-                      className={`${style.imagesize} img-fluid mb-3 mb-lg-0`}
-                      src={painting.src}
-                      alt={painting.title}
-                    />
+            <h1 className={style.detailtitle}>{painting.title}</h1>
+            
+              <div className="row">
+                <div className="col-sm-4">
+                  <div className='imagecontainer'>
+                      <img
+                        className={`${style.imagesize} img-fluid mb-3 mb-lg-0`}
+                        src={painting.src}
+                        alt={painting.title}
+                      />
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-7 col-md-7 col-sm-6">
-                <h1 className={style.details}>Hello details here</h1>
-              </div>
+
+                <div className={`${style.infoPaintingContainer} col-sm-8`}>
+                  <div>
+                    <h3 className={style.details}>Hello details here</h3>
+                      <div className="row">
+                        <div className="col-12">
+                          <p className='text-white'>{painting.size}</p>
+                        </div>
+                      </div>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
         
-    
+        </div>
     </div>
     </>
   );
--- myrakel/src/data/paintingsData.js
+++ myrakel/src/data/paintingsData.js
@@ -4,7 +4,7 @@ import Tavla3 from '../images/tavlaröd.jpg';
 import Tavla4 from '../images/tavlama.jpg';
 
 const paintingsData = [
-  { id: 1, src: Tavla1, title: "Green Forrest" },
+  { id: 1, src: Tavla1, title: "Green Forrest", size: "100x120" },
   { id: 2, src: Tavla2, title: "Brownie" },
   { id: 3, src: Tavla3, title: "Red Love" },
   { id: 4, src: Tavla4, title: "Swirl" },
